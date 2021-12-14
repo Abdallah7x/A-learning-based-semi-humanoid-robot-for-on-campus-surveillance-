bf4e9f1db8032e5823057745f4966ea8c35e7e3f
Pierre Malak <69065062+Pierre-Malak@users.noreply.github.com>
2021-12-14 22:10:11 +0200

    Update Main.py

--- ExcelReading/Main.py
+++ ExcelReading/Main.py
@@ -1,11 +1,14 @@
 import sys
   
 # adding Folder_2 to the system path
-sys.path.insert(0, 'F:/Senior Year/GP/Proposal/ExcelReading')
-  
-# importing the add and odd_even 
-# function
+sys.path.insert(0, 'E:/CS/Graduation Project/-A-learning-based-semi-Humanoid-Robot-for-On-campus-Surveillance-main/ExcelReading')
+sys.path.insert(0, 'E:/CS/Graduation Project/-A-learning-based-semi-Humanoid-Robot-for-On-campus-Surveillance-main/VoiceCommands')
+
 from ExcelReading import ReadFileData_Time
-  
-# calling odd_even function
-ReadFileData_Time("Pierre Malak" ,"Sunday", "14")
+from Project_VoiceCommands import begin
+
+#ReadFileData_Time("Pierre Malak" ,"Sunday", "14")
+
+if __name__ == '__main__':
+    
+    begin()
