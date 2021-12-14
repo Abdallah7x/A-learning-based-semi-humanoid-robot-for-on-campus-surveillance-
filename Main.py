eb05ed5a56f25c129b685afd6f2ede421f9d738a
Pierre Malak <69065062+Pierre-Malak@users.noreply.github.com>
2021-12-14 22:19:28 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,8 +1,8 @@
 import sys
   
 # adding Folder_2 to the system path
-sys.path.insert(0, 'E:/CS/Graduation Project/-A-learning-based-semi-Humanoid-Robot-for-On-campus-Surveillance-main/ExcelReading')
-sys.path.insert(0, 'E:/CS/Graduation Project/-A-learning-based-semi-Humanoid-Robot-for-On-campus-Surveillance-main/VoiceCommands')
+sys.path.insert(0, ':/ExcelReading')
+sys.path.insert(0, ':/VoiceCommands')
 
 from ExcelReading import ReadFileData_Time
 from Project_VoiceCommands import begin
