5a31f497bc0e340d43534a12d47bb04607438e4c
Abdallah7x <76527450+Abdallah7x@users.noreply.github.com>
2022-02-18 19:29:08 +0200

    Add files via upload

---
+++ tablet Application/lib/data/users.dart
@@ -0,0 +1,39 @@
+import 'package:tablet/model/user.dart';
+
+final allUsers = <User>[
+  User(
+      firstName: 'Abdallah khyrat',
+      ID: '2018/1xxx',
+      Ticketdate: "20/2/2022",
+      Tickettype: "Playing cards"),
+  User(
+      firstName: 'adel ahmed',
+      ID: '2018/1xxx',
+      Ticketdate: "25/1/2022",
+      Tickettype: "Fighting"),
+  User(
+      firstName: 'Alaa sayed',
+      ID: '2018/1xxx',
+      Ticketdate: "22/4/2022",
+      Tickettype: "Playing cards"),
+  User(
+      firstName: 'sameh shokry',
+      ID: '2018/1xxx',
+      Ticketdate: "22/3/2022",
+      Tickettype: "Playing cards"),
+  User(
+      firstName: 'Seif ahmed',
+      ID: '2018/1xxx',
+      Ticketdate: "25/2/2022",
+      Tickettype: "Fighting"),
+  User(
+      firstName: 'Omar ali',
+      ID: '2019/1xxx',
+      Ticketdate: "10/4/2022",
+      Tickettype: "Playing cards"),
+  User(
+      firstName: 'sara ali',
+      ID: '2018/1xxx',
+      Ticketdate: "5/7/2022",
+      Tickettype: "Fighting"),
+];
