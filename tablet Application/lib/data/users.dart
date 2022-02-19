fcbdbe9e567faae8b63f4a6b382ef5b1e47a4fae
Abdallah7x <76527450+Abdallah7x@users.noreply.github.com>
2022-02-20 00:14:55 +0200

    Update users.dart

--- tablet Application/lib/data/users.dart
+++ tablet Application/lib/data/users.dart
@@ -2,15 +2,68 @@ import 'package:tablet/model/user.dart';
 
 final allUsers = <User>[
   User(
-      firstName: 'Abdallah khyrat',
-      ID: '2018/1xxx',
-      Ticketdate: "20/2/2022",
-      Tickettype: "Playing cards"),
-  User(
       firstName: 'adel ahmed',
       ID: '2018/1xxx',
       Ticketdate: "25/1/2022",
       Tickettype: "Fighting"),
+  User(
+      firstName: 'asmaa zozoo',
+      ID: '2018/1xxx',
+      Ticketdate: "5/7/2022",
+      Tickettype: "Fighting"),
+  User(
+      firstName: 'hossam tarek ali',
+      ID: '2018/1xxx',
+      Ticketdate: "5/7/2022",
+      Tickettype: "Fighting"),
+  User(
+      firstName: 'zaki fathi',
+      ID: '2018/1xxx',
+      Ticketdate: "5/7/2022",
+      Tickettype: "Fighting"),
+  User(
+      firstName: 'Hassab omar',
+      ID: '2018/1xxx',
+      Ticketdate: "5/7/2022",
+      Tickettype: "Fighting"),
+  User(
+      firstName: 'salma tamer',
+      ID: '2018/1xxx',
+      Ticketdate: "5/7/2022",
+      Tickettype: "Fighting"),
+];
+
+final allUsers2 = <User>[
+  User(
+      firstName: 'Seif ahmed',
+      ID: '2018/1xxx',
+      Ticketdate: "25/2/2022",
+      Tickettype: "Smoking"),
+  User(
+      firstName: 'Fares awad',
+      ID: '2018/1xxx',
+      Ticketdate: "25/2/2022",
+      Tickettype: "Smoking"),
+  User(
+      firstName: 'safi rostom',
+      ID: '2018/1xxx',
+      Ticketdate: "25/2/2022",
+      Tickettype: "Smoking"),
+];
+
+final allUsers3 = <User>[
+  User(
+    firstName: 'Abdallah khyrat',
+    ID: '2018/1xxx',
+    Ticketdate: "20/2/2022",
+    Tickettype: "Playing cards",
+  ),
+  User(
+    firstName: 'Ahmed  zaki',
+    ID: '2018/1xxx',
+    Ticketdate: "30/12/2022",
+    Tickettype: "Playing cards",
+  ),
   User(
       firstName: 'Alaa sayed',
       ID: '2018/1xxx',
@@ -22,18 +75,8 @@ final allUsers = <User>[
       Ticketdate: "22/3/2022",
       Tickettype: "Playing cards"),
   User(
-      firstName: 'Seif ahmed',
-      ID: '2018/1xxx',
-      Ticketdate: "25/2/2022",
-      Tickettype: "Fighting"),
-  User(
       firstName: 'Omar ali',
       ID: '2019/1xxx',
       Ticketdate: "10/4/2022",
       Tickettype: "Playing cards"),
-  User(
-      firstName: 'sara ali',
-      ID: '2018/1xxx',
-      Ticketdate: "5/7/2022",
-      Tickettype: "Fighting"),
 ];
