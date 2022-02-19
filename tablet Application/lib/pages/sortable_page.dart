fbc633faf31adf11784b37a49c2468cc92f58ed3
Abdallah7x <76527450+Abdallah7x@users.noreply.github.com>
2022-02-19 14:02:18 +0200

    Update sortable_page.dart

--- tablet Application/lib/pages/sortable_page.dart
+++ tablet Application/lib/pages/sortable_page.dart
@@ -12,6 +12,9 @@ class _SortablePageState extends State<SortablePage> {
   late List<User> users;
   int? sortColumnIndex;
   bool isAscending = false;
+   void _doSomething() {
+    showAlertDialog(context);
+  }
 
   @override
   void initState() {
@@ -85,4 +88,56 @@ class _SortablePageState extends State<SortablePage> {
 
   int compareString(bool ascending, String value1, String value2) =>
       ascending ? value1.compareTo(value2) : value2.compareTo(value1);
+  
+  AlertDialog alert = AlertDialog(
+      title: Center(child: Text('Student Ticket')),
+      content: Row(
+        children: [
+          Container(
+            height: 400,
+            width: 370,
+            child: Image.asset(
+              "assets/images/ss.jpeg",
+              fit: BoxFit.fill,
+            ),
+          ),
+          Column(
+            children: [
+              SizedBox(
+                height: 130,
+              ),
+              Text("student name: Seif Ahmed"),
+              Text("student ID: 2018/1xxx"),
+              SizedBox(
+                height: 30,
+              ),
+              Text("Ticket type:  Smoking"),
+              SizedBox(
+                height: 5,
+              ),
+              Text("Ticket Date: 20/2/2022"),
+              SizedBox(
+                height: 5,
+              ),
+              Text("Ticket time:2:32 PM"),
+              SizedBox(
+                width: 500,
+              ),
+            ],
+          ),
+        ],
+      ),
+      actions: [
+        okButton,
+      ],
+    );
+
+    // show the dialog
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return alert;
+      },
+    );
+}
 }
