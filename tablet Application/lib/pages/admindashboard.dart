7f35b91285df285a5bfa3aa3001415a8437e43a2
seifeldinahmed731 <83872282+seifeldinahmed731@users.noreply.github.com>
2022-03-07 21:23:37 +0200

    Update admindashboard.dart

--- tablet Application/lib/pages/admindashboard.dart
+++ tablet Application/lib/pages/admindashboard.dart
@@ -145,6 +145,7 @@ class Admindashboard extends StatelessWidget {
                ),
                 ],
              ),
+             
             ),
          ],
           ),
@@ -161,7 +162,37 @@ class Admindashboard extends StatelessWidget {
                 
             ),
           
-        
+        bottomNavigationBar: BottomNavigationBar(
+          //currentIndex: _selectedIndex,
+          type: BottomNavigationBarType.fixed,
+          //backgroundColor: Colors.transparent,
+          selectedFontSize: 22,
+          unselectedFontSize: 15,
+          selectedItemColor: Colors.red.shade800,
+          items: [
+            BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                label: "Home",
+                backgroundColor: Colors.red.shade800),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.search),
+                label: "Search",
+                backgroundColor: Colors.red.shade800),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.person),
+                label: "Q/A",
+                backgroundColor: Colors.red.shade800),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.camera),
+                label: "Rate us",
+                backgroundColor: Colors.red.shade800),
+          ],
+          onTap: (index) {
+            // setState(() {
+            //  // _selectedIndex = index;
+            // });
+          },
+        )
       );
     
   }
