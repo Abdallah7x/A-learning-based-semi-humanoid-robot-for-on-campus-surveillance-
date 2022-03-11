b2181a047c632581ad7d8267002d654c5750294c
seifeldinahmed731 <83872282+seifeldinahmed731@users.noreply.github.com>
2022-03-11 22:05:49 +0200

    Create users1.dart

---
+++ tablet Application/lib/data/users1.dart
@@ -0,0 +1,82 @@
+import 'package:tablet_app/model/user.dart';
+
+final allUsers = <User>[
+  User(
+      firstName: 'N bulding',
+      ID: 'N61',
+      Ticketdate: "25/1/2022",
+      Tickettype: "Mobile Development"),
+  User(
+      firstName: 'R bulding',
+      ID: 'RC1',
+      Ticketdate: "22/1/2022",
+      Tickettype: "Data Structure"),
+  User(
+      firstName: 'Main building',
+      ID: '242',
+      Ticketdate: "2/2/2022",
+      Tickettype: "Introduction to cs"),
+  User(
+      firstName: 'S bulding',
+      ID: 'S16',
+      Ticketdate: "20/1/2022",
+      Tickettype: "Selected topics"),
+  User(
+      firstName: 'N bulding',
+      ID: 'N27',
+      Ticketdate: "5/7/2022",
+      Tickettype: "Math 1"),
+  User(
+      firstName: 'R bulding',
+      ID: 'RE7',
+      Ticketdate: "5/7/2022",
+      Tickettype: "Physics"),
+];
+
+final allUsers2 = <User>[
+  User(
+      firstName: 'Seif ahmed',
+      ID: '2018/1xxx',
+      Ticketdate: "25/2/2022",
+      Tickettype: "Smoking"),
+  User(
+      firstName: 'Seif ahmed',
+      ID: '2018/1xxx',
+      Ticketdate: "25/2/2022",
+      Tickettype: "Smoking"),
+  User(
+      firstName: 'Seif ahmed',
+      ID: '2018/1xxx',
+      Ticketdate: "25/2/2022",
+      Tickettype: "Smoking"),
+];
+
+final allUsers3 = <User>[
+  User(
+    firstName: 'Seif ahmed',
+    ID: '2018/1xxx',
+    Ticketdate: "20/2/2022",
+    Tickettype: "Playing cards",
+  ),
+  User(
+    firstName: 'Seif ahmed',
+    ID: '2018/1xxx',
+    Ticketdate: "30/12/2022",
+    Tickettype: "Playing cards",
+  ),
+  User(
+      firstName: 'Seif ahmed',
+      ID: '2018/1xxx',
+      Ticketdate: "22/4/2022",
+      Tickettype: "Playing cards"),
+  User(
+      firstName: 'Seif ahmed',
+      ID: '2018/1xxx',
+      Ticketdate: "22/3/2022",
+      Tickettype: "Playing cards"),
+  User(
+      firstName: 'Seif ahmed',
+      ID: '2019/1xxx',
+      Ticketdate: "10/4/2022",
+      Tickettype: "Playing cards"),
+];
