1f8ec78763d939562c11a2691e4a6244b62288cb
seifeldinahmed731 <83872282+seifeldinahmed731@users.noreply.github.com>
2022-05-10 18:14:03 +0200

    Create footer.php

---
+++ web/screens/footer.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+<!-- <link rel="stylesheet" type="text/css" href="css/stylesheet.css"> -->
+<link rel="stylesheet" type="text/css" href="css/history.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap" rel="stylesheet">
+	<title></title>
+</head>
+<body>
+ <div>
+  <section id="footer">
+          <!-- Site footer -->
+    <footer class="site-footer" style="margin-left: -261px;width: 1579px; ">
+        <div class="container">
+          <div class="row">
+            <div class="col-sm-12 col-md-6">
+              <h6>About</h6>
+              <p class="text-justify">Team 22 <i>S.P.A.M </i> Graduation project <br> 
+            Abdallah, Pierre, Maryam, Seif <br>
+        Supervised by: Haytham Metawei, Verina Saber, Mohamed EL Shalakani            </p>
+            </div>
+  
+            <div class="col-xs-6 col-md-3">
+              <h6>Contact</h6>
+              <ul class="footer-links contact-info">
+                <li><a href=""><i class="fas fa-envelope"></i> miu@miuegypt.edu.eg</a></li>
+                <li><a href=""><i class="fa fa-phone" aria-hidden="true"> 19-MIU / 19648</i></a></li>
+                <li><a href=""><i class="fa fa-fax" aria-hidden="true"> 	(+202) 24772038</i></a></li>
+                <li><a href=""><i class="fa fa-address-card" aria-hidden="true"></i> 	KM 28 Cairo – Ismailia Road Ahmed Orabi District, Cairo – Egypt</a></li>
+                <li><a href=""></a></li>
+              </ul>
+            </div>
+  
+            <div class="col-xs-6 col-md-3">
+              <!-- <h6>Quick Links</h6>
+              <ul class="footer-links">
+                <li><a href="">Events</a></li>
+                <li><a href="">Profile</a></li>
+                <li><a href="">Contact Us</a></li>
+                <li><a href="">Privacy Policy</a></li>
+              </ul> -->
+            </div>
+          </div>
+          <hr>
+        </div>
+        <div class="container last">
+          <div class="row">
+            <div class="col-md-8 col-sm-6 col-xs-12">
+              <p class="copyright-text"> Made with <i class="fa fa-heart" aria-hidden="true"></i> by
+                    <a href="#">S.P.A.M Team 22 </a>
+              </p>
+            </div>
+  
+            <div class="col-md-4 col-sm-6 col-xs-12">
+              <!-- <ul class="social-icons">
+                <li><a class="facebook" href="#"><i class="fab fa-facebook"></i></a></li>
+                <li><a class="twitter" href="#"><i class="fab fa-twitter"></i></a></li>
+                <li><a class="dribbble" href="#"><i class="fab fa-instagram"></i></a></li>
+                <li><a class="linkedin" href="#"><i class="fab fa-linkedin"></i></a></li>   
+              </ul> -->
+            </div>
+          </div>
+        </div>
+  </footer>
+    </section>
+        </div>
+</body>
+</html>
