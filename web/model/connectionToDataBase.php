72aa131e3e86d8d35b47b4eff4d18b44f3283703
seifeldinahmed731 <83872282+seifeldinahmed731@users.noreply.github.com>
2022-05-13 17:30:48 +0200

    Update connectionToDataBase.php

--- web/model/connectionToDataBase.php
+++ web/model/connectionToDataBase.php
@@ -1,1 +1,18 @@
+<?php
+
+class connectionToDatabase  {
+           public $servername = "localhost";
+           public $username = "root";
+           public $password = "";
+           public $dbname = "portal";
+
+
+           public  function ConnectToDataBase(){
+           $conn = new mysqli($this->servername, $this->username, $this->password, $this->dbname);
+           return $conn;
+
+           }
+        }
+
+?>
 
