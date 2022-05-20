18b93245d40d820d51aedda4465b7683b0c4dcf7
seifeldinahmed731 <83872282+seifeldinahmed731@users.noreply.github.com>
2022-05-20 16:32:39 +0200

    Create homepageee.dart

---
+++ tablet Application/lib/pages/homepageee.dart
@@ -0,0 +1,248 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_neumorphic/flutter_neumorphic.dart';
+// import 'package:tablet/pages/Navigation.dart';
+import 'package:clay_containers/clay_containers.dart';
+// import 'package:tablet/pages/picture.dart';
+// import 'package:tablet/pages/schedule.dart';
+// import 'package:tablet/pages/test.dart';
+// import 'package:tablet/pages/tick.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  bool _initialValue = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.red.shade800,
+        title: Image.asset(
+          "assets/logo/miu-logo.jpg",
+          width: 120,
+          height: 55,
+        ),
+      ),
+      body: Align(
+        alignment: Alignment.centerLeft,
+        child: LayoutBuilder(
+          builder: (BuildContext context, BoxConstraints constraints) {
+            return Container(
+              decoration: BoxDecoration(                     
+                        borderRadius: BorderRadius.circular(25), 
+                        color: Colors.black.withOpacity(0.8),              
+                      ),
+                      margin: EdgeInsets.only(left: 70,top: 50),
+              child: Row(
+                children: [
+                  SizedBox(
+                    width: 35,
+                  ),
+                  Column(
+                    children: [
+                      Image.asset(
+                        "assets/logo/pp.png",
+                        height: 580,
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    width: 80,
+                  ),
+                  Column(
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => HomePage()),
+                          );
+                        },
+                        child: Column(
+                          children: [
+                            SizedBox(
+                              height: 20,
+                            ),
+                            Row(
+                              children: [
+                                Center(
+                                  child: ClayContainer(
+                                    color: Color(0xFFF2F2F2),
+                                    height: 200,
+                                    width: 210,
+                                    borderRadius: 50,
+                                    child: Image.asset(
+                                      "assets/images/schedules.jpg",
+                                      fit: BoxFit.fill,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                Text(
+                                  'View schedule',
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                      fontSize: 35.0,
+                                      fontFamily: 'Roboto',
+                                      color: Colors.grey),
+                                ),
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                        //   Navigator.push(
+                        //     context,
+                        //     // MaterialPageRoute(
+                        //     //     //builder: (context) => navigation()
+                        //     //     ),
+                        //   );
+                        },
+                        child: Column(
+                          children: [
+                            Row(
+                              children: [
+                                Center(
+                                  child: ClayContainer(
+                                    color: Color(0xFFF2F2F2),
+                                    height: 200,
+                                    width: 210,
+                                    borderRadius: 50,
+                                    child: Image.asset(
+                                      "assets/images/nav.jpg",
+                                      fit: BoxFit.fill,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                Text(
+                                  'Navigation',
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                      fontSize: 35.0,
+                                      fontFamily: 'Roboto',
+                                      color: Colors.grey),
+                                ),
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    width: 150,
+                  ),
+                  Column(
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          // Navigator.push(
+                          //   context,
+                          //   MaterialPageRoute(builder: (context) => picture()),
+                          // );
+                        },
+                        child: Column(
+                          children: [
+                            SizedBox(
+                              height: 30,
+                            ),
+                            Row(
+                              children: [
+                                Center(
+                                  child: ClayContainer(
+                                    color: Color(0xFFF2F2F2),
+                                    height: 200,
+                                    width: 210,
+                                    borderRadius: 50,
+                                    child: Image.asset(
+                                      "assets/images/picture.jpg",
+                                      fit: BoxFit.fill,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                Text(
+                                  'Take picture',
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                      fontSize: 35.0,
+                                      fontFamily: 'Roboto',
+                                      color: Colors.grey),
+                                ),
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          // Navigator.push(
+                          //   context,
+                          //   MaterialPageRoute(builder: (context) => MainPage()),
+                          // );
+                        },
+                        child: Column(
+                          children: [
+                            Row(
+                              children: [
+                                Center(
+                                  child: ClayContainer(
+                                    color: Color(0xFFF2F2F2),
+                                    height: 200,
+                                    width: 210,
+                                    borderRadius: 50,
+                                    child: Image.asset(
+                                      "assets/images/t.jpg",
+                                      fit: BoxFit.fill,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                Text(
+                                  'Ticket history',
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                      fontSize: 35.0,
+                                      fontFamily: 'Roboto',
+                                      color: Colors.grey),
+                                ),
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
